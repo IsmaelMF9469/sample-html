14b929991a12dd2e5f074484d6fcc640ea1b52c8
IsmaelMF9469 <ismaelmelo9469@hotmail.com>
2022-11-17 12:41:54 -0300

    ..

---
+++ script.js
@@ -0,0 +1,13 @@
+function handleClick () {
+    document.querySelector('#Titulo').innerHTML = 'Cleitin';
+}
+
+function handleMove () {
+    document.querySelector('#Titulo').innerHTML = 'Cleitin';
+}
+
+function handleOut () {
+    document.querySelector('#Titulo').innerHTML = 'Ismael';
+}
+
+console.log("Ainda estou aqui cara")
